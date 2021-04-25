21258767b5d81b505477d728ab89842e370c42c5
Jonas Wahringer <jonas.wahringer@gmail.com>
2021-04-25 02:46:00 +0200

    updated update

---
+++ src/CheckLoginData.java
@@ -0,0 +1,51 @@
+public class CheckLoginData {
+    String inputName, inputPassword;
+    DatabaseConnection dbc;
+    String uname;
+    int hsc;
+
+    public CheckLoginData(String n, String pw) {
+        this.inputName = n;
+        this.inputPassword = pw;
+
+        getDataFromDB();
+        checkCredentials();
+
+    }
+
+    public void getDataFromDB() {
+        dbc = new DatabaseConnection();
+    }
+
+    public void checkCredentials() {
+        String[] checkNames = dbc.getUserNames();
+        String[] checkPW = dbc.getUserPasswords();
+        int[] highscores = dbc.getHighscores();
+
+        for(int i=0; i<checkNames.length; i++) {
+            if(checkNames[i].equals(inputName) & checkPW[i].equals(inputPassword)) {
+                setCheckedHighscore(highscores[i]);
+                setCheckedUname(checkNames[i]);
+                break;
+            }
+        }
+        
+    }
+
+    public void setCheckedHighscore(int hsc) {
+        this.hsc = hsc;
+    }
+
+    public int getCheckedHighscore() {
+       return hsc;
+    }
+
+    public void setCheckedUname(String uname) {
+        this.uname = uname;
+    }
+
+    public String getCheckedUname() {
+       return uname;
+    }
+
+}
---
+++ src/Login.java
@@ -0,0 +1,95 @@
+import java.awt.event.ActionEvent;
+
+import javax.swing.AbstractAction;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JPasswordField;
+import javax.swing.JTextField;
+
+
+public class Login extends JFrame {
+    GameWindow gameWindow;
+    CheckLoginData cld;
+    JLabel userLabel, passwordLabel, message;
+    JTextField userNameText, passwordText;
+    JButton submitButton;
+    JPanel panel;
+    String inputName, inputPassword;
+    int checkedHighscore;
+    String checkedUname;
+
+    public Login() {
+        super("Login System");
+        this.setBounds(400,200,250,150);
+        this.setDefaultCloseOperation(DISPOSE_ON_CLOSE);
+        initFile();
+
+        //user label
+        userLabel = new JLabel();
+        userLabel.setText("User inputName :");
+        userNameText = new JTextField();
+        userNameText.setText("Please enter your name");
+
+        // Password Label
+        passwordLabel = new JLabel();
+        passwordLabel.setText("inputPassword :");
+        passwordText = new JPasswordField();
+        passwordText.setText("12345");
+
+        // Submit
+        submitButton = new JButton("SUBMIT");
+        submitButton.addActionListener(new submitButtonListener());
+        
+        panel = new JPanel();
+        panel.add(userLabel);
+        panel.add(userNameText);
+        panel.add(passwordLabel);
+        panel.add(passwordText);
+
+        message = new JLabel();
+        panel.add(message);
+        panel.add(submitButton);
+        
+        this.add(panel);
+
+        this.setVisible(true);
+    }
+
+    public class submitButtonListener extends AbstractAction {
+
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            getCredentials();
+            cld = new CheckLoginData(inputName, inputPassword);
+            checkedHighscore = cld.getCheckedHighscore();     
+            checkedUname = cld.getCheckedUname();
+            close();
+        }
+
+	}
+    
+    public void close() {
+        this.dispose();
+    }
+
+    public void initFile() {
+
+    }
+
+    public String getFinalUsername() {
+        return checkedUname;
+    }
+
+    public int getFinalHighscore() {
+        return checkedHighscore;
+    }
+
+
+
+    public void getCredentials() {
+        this.inputName = userNameText.getText();
+        this.inputPassword = passwordText.getText();
+    }
+}
